533a2f025b6d767c1af3c8b5c1ddbf1b871f9f4f
BeltranJ128 <143763139+BeltranJ128@users.noreply.github.com>
2024-03-01 13:34:20 -0700

    Update pantalla2.dart

--- lib/pantalla2.dart
+++ lib/pantalla2.dart
@@ -7,6 +7,7 @@ class Pantalla2 extends StatelessWidget {
   Widget build(BuildContext context) {
     final argumento = ModalRoute.of(context)!.settings.arguments as String;
     return Scaffold(
+      backgroundColor: Colors.brown[100],
       appBar: AppBar(
         title: const Text("Pantalla2 Jose Beltran"),
       ),
@@ -15,11 +16,30 @@ class Pantalla2 extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(argumento),
+            Icon(
+              Icons.apple,
+              size: 200,
+            ),
             ElevatedButton(
               child: const Text("Regresar a la pantalla 1"),
               onPressed: () {
                 Navigator.pop(context);
               },
+            ),
+            Container(
+              width: double.infinity,
+              alignment: Alignment.center,
+              child: Card(
+                margin: EdgeInsets.all(32),
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text(
+                    "Hola mundo 2!",
+                    style: TextStyle(fontSize: 30, color: Colors.white),
+                  ),
+                ),
+                color: Colors.brown,
+              ),
             )
           ],
         ),
